89c25b09d4dc13883ee9ef3444b9033e57c32507
mozix5 <mosinmd827@gmail.com>
2024-10-18 14:24:52 +0530

    add shadcn

--- pet-pal-ui/src/App.tsx
+++ pet-pal-ui/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
-    <div className="bg-slate-600">App</div>
-  )
-}
+    <div className="bg-slate-600">
+      App
+      <Button>hello world</Button>
+    </div>
+  );
+};
 
-export default App
+export default App;
